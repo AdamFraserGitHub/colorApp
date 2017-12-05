2923e90fa40d303dc0371d03621a8c7f4d18cb89
Adam <AdamFraserPrograming@gmail.com>
2017-12-05 12:57:28 +0000

    commit

--- server working/msgs/app.js
+++ server working/msgs/app.js
@@ -1,12 +1,12 @@
 var express = require('express')
-
-, http = require('http'), path = require('path');
-
+var http = require('http'), path = require('path');
 var app = express();
-
 var bodyParser = require('body-parser');
 
-var messages = [];
+var color = {
+    'r' : 255,
+    'g' : 0
+};
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
@@ -14,13 +14,15 @@ app.use(bodyParser.json());
 /*to access the posted data from client using request bpdy*/
 app.post('/post', function (req, res) {
     /* Handling the AngularJS post request*/
-    console.log(req);
-    messages.push(req.body.textdata);
+    console.log(req.host);
+    if(req.body.command == "gPlus") {
+        color.g+=5;
+    }
     res.setHeader('Content-Type', 'application/json');
     /*response has to be in the form of a JSON*/
     // req.body.serverMessage = "NodeJS replying to angular"
         /*adding a new field to send it to the angular Client */
-    res.end(messages.toString());
+    res.end(color.toString());
     /*Sending the respone back to the angular Client */
 });
 
